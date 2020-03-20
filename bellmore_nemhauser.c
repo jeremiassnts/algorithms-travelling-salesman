3d9948f61ea9413c2f345fb52bec40bb7dee513e
Jeremias Santos <jeremiassnts3@gmail.com>
2020-03-19 23:42:58 -0300

    heuristica Bellmore & Nemhause implementada

---
+++ bellmore_nemhauser.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include "matrix.c"
+#include "list.c"
+
+int main()
+{
+    //nome do arquivo & quantidade de linhas/colunas
+    char *fileName = (char *)malloc(sizeof(char) * 1000);
+    int n;
+    scanf("%s %d", fileName, &n);
+
+    double **matrix = readMatrix(fileName, n);
+    list *Hls = newList();
+    //insere primeiro vertice
+    add(Hls, 1);
+    //itera até visitar todos vértices
+    while (Hls->size < n)
+    {
+        int w = near(Hls, n, matrix);
+        add(Hls, w);
+    }
+    //fecha o ciclo com o vértice inicial
+    add(Hls, 1);
+
+    printList(Hls);
+    double cost = sumCost(Hls, matrix);
+    printf("\ncusto minimo -> %lf", cost);
+
+    free(matrix);
+    free(Hls);
+    free(fileName);
+    return 0;
+}
